75128721c8e80d3c57a1af1cebe7ad19820959f1
pearlMat <pearllyt@gmail.com>
2023-10-16 13:31:48 +0100

    created nav partial file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,14 +13,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.html",
+	}
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
