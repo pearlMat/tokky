a26fccf63ad61f469b5bdb306cdf2dbabbeb58ed
pearlMat <pearllyt@gmail.com>
2023-10-18 21:33:57 +0100

    modified main func

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,10 @@ import (
 // The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
